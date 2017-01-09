fd19dcac03f140b63eb2379f6628971c8a5ff413
tkatchev <tkatchev@gmail.com>
2017-01-09 17:37:03 +0300

    judy: keep static libraries.

--- pkgs/development/libraries/judy/default.nix
+++ pkgs/development/libraries/judy/default.nix
@@ -8,6 +8,8 @@ stdenv.mkDerivation {
     sha256 = "1sv3990vsx8hrza1mvq3bhvv9m6ff08y4yz7swn6znszz24l0w6j";
   };
 
+  dontDisableStatic = true;
+
   # gcc 4.8 optimisations break judy.
   # http://sourceforge.net/p/judy/mailman/message/31995144/
   preConfigure = stdenv.lib.optionalString stdenv.cc.isGNU ''
